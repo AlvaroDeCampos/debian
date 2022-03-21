486e1b71bc416693a8d149d688bd891fd68b1f44
AlvaroDeCampos <102044654+AlvaroDeCampos@users.noreply.github.com>
2022-03-21 13:06:17 -0300

    Add files via upload

---
+++ pos-install.sh
@@ -0,0 +1,15 @@
+sudo apt install youtube-dl \
+                 ranger \
+                 polybar \
+                 rxvt-unicode \
+                 xsel \
+                 qbittorrent \
+                 redshift \
+                 breeze-cursor-theme \
+                 evince \
+                 xinput \
+                 scrot \
+                 light \
+                 tlp \
+                 fonts-terminus-otb
+
